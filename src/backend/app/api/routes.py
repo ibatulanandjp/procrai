728159933d09ecd086af0baa456b707d6710178e
Atul Anand <ib.atulanandjp@gmail.com>
2025-03-13 01:15:01 +0900

    feat: backend setup
    Flask backend setup with a health endpoint, and necessary linting and formatting.

--- src/backend/app/api/routes.py
+++ src/backend/app/api/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify
 
-api_blueprint = Blueprint('api', __name__)
+api_blueprint = Blueprint("api", __name__)
 
-@api_blueprint.route('/health', methods=['GET'])
+
+@api_blueprint.route("/health", methods=["GET"])
 def health():
-    return jsonify({'health': 'API is up and running!'})
+    return jsonify({"health": "API is up and running!"})
--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,19 +1,23 @@
-from flask import Flask
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from flask import Flask
+
 load_dotenv()
+
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'my_best_kept_secret')
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "my_best_kept_secret")
 
     from api.routes import api_blueprint
-    app.register_blueprint(api_blueprint, url_prefix='/api')
+
+    app.register_blueprint(api_blueprint, url_prefix="/api")
 
     return app
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
