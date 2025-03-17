1da901d6f9512114b82f11d8d85329488820da40
Atul Anand <ib.atulanandjp@gmail.com>
2025-03-18 02:14:12 +0900

    feat: setup backend structure

--- src/backend/app/api/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-api_blueprint = Blueprint("api", __name__)
-
-
-@api_blueprint.route("/health", methods=["GET"])
-def health():
-    return jsonify({"health": "API is up and running!"})
