24a0b44352ff5ca07bfe84c3d30bc70f5a456c28
Atul Anand <ib.atulanandjp@gmail.com>
2025-03-22 20:09:20 +0900

    feat: added ocr feature

--- src/backend/app/api/v1/api_router.py
+++ src/backend/app/api/v1/api_router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.v1.routes import base, upload
+from app.api.v1.routes import base, ocr, upload
 
 api_router = APIRouter()
 api_router.include_router(base.router, tags=["base"])
 api_router.include_router(upload.router, tags=["upload"], prefix="/upload")
+api_router.include_router(ocr.router, tags=["ocr"], prefix="/ocr")
